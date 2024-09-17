84e80c2bc017570fe2e306e3596f6d6161554441
nathannwangg <nathannwangg@gmail.com>
2024-09-16 21:28:33 -0500

    Initial Commit

---
+++ time-travel/time_travelApp.swift
@@ -0,0 +1,17 @@
+//
+//  time_travelApp.swift
+//  time-travel
+//
+//  Created by Nathan Wang on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct time_travelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
