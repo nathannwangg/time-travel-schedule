d07d098ed633a1c0efd9c7f0e44684f31bdd38e0
nathannwangg <nathannwangg@gmail.com>
2024-09-17 20:34:41 -0500

    Created APIConstants, defining the base URL and endpoints for API requests.

---
+++ time-travel/Network/APIConstants.swift
@@ -0,0 +1,13 @@
+//
+//  APIConstants.swift
+//  time-travel
+//
+//  Created by Nathan Wang on 9/16/24.
+//
+
+import Foundation
+
+struct APIConstants {
+    static let baseURL = URL(string: "https://adonix.hackillinois.org/event/")! // Initialize the base URL of the API
+    static let eventsEndpoint = "/event" // Initialize endpoint for fetching all events
+}
